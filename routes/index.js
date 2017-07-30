f866ddddd38bb87f19bd1f61bae920d715de1e3c
anfelo <af.osorio1341@gmail.com>
2017-07-29 22:25:34 -0500

    Added yelp-fusion API search

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var yelp = require('yelp-fusion');
+
+var clientId = 'DB3MWYqRCvWCKg5E-hs-SA';
+var clientSecret = 'L0fDXzoPl2NZQRnb6BwMtIhEU5GSuSGRC1I6ly1L1U1WhqrXgkgQZZg9dnCLYCMb';
+
+var searchRequest = {
+  term: 'Bars',
+  location: '',
+  categories: 'nightlife'
+};
 
 // GET / 
 // Home Route
@@ -9,13 +19,25 @@ router.get('/', function(req, res, next){
 // POST /
 // Search for bars in the specified city
 router.post('/', function(req, res, next){
-	// Request bars to Yelp API
-	res.redirect('/search');
+	
 });
 // GET /search
 // Display Search
-router.get('/search', function(req, res, next){
-	res.send('Hello from /serach');
+router.post('/search', function(req, res, next){
+	// Request bars to Yelp API
+	searchRequest.location = req.body.city.toLowerCase();
+	yelp.accessToken(clientId, clientSecret).then(response => {
+  var client = yelp.client(response.jsonBody.access_token);
+
+		client.search(searchRequest).then(response => {
+			var results = response.jsonBody.businesses;
+			res.json(results);
+		}).catch(e => {
+				next(e);
+		});
+	}).catch(e => {
+		next(e);
+	});
 });
 
 module.exports = router;
